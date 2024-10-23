c860fa67dbfaf256a18b42296c4dfd34c45cf19f
meowmeowahr <meowmeowahr@gmail.com>
2024-10-22 23:55:44 -0500

    Begin work on click cli interface

--- src/kevinbotlib/launch.py
+++ src/kevinbotlib/launch.py
@@ -1,3 +1,34 @@
-def kevinbot():
-    print("Welcome to Kevinbot!")  # noqa: T201
-    print("This will be the future executable for the KevinbotLib MQTT and XBee interface")  # noqa: T201
+import click
+
+
+@click.group(
+    context_settings={'help_option_names': ['-h', '--help'], 'max_content_width': 120}
+)
+def cli():
+    """Kevinbotlib command-line interface"""
+    pass
+
+@click.command()
+def serve():
+    """Start the Kevinbot MQTT inferface"""
+    click.echo("In the future...")
+
+@click.command()
+def listen():
+    """Listen to MQTT topic(s)"""
+    click.echo("In the future...")
+
+@click.command()
+@click.argument('topic')
+@click.argument('message')
+def pub(_, __):
+    """Publish a message to a specific MQTT topic"""
+    click.echo("In the future...")
+
+# Add commands to the main CLI group
+cli.add_command(serve)
+cli.add_command(listen)
+cli.add_command(pub)
+
+if __name__ == "__main__":
+    cli()
