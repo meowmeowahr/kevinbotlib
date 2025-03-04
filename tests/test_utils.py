f74469a0abfcccfdf4b4e441a3bfe7b3d5bac45a
meowmeowahr <meowmeowahr@gmail.com>
2025-03-04 12:37:38 -0600

    fullclassname test

---
+++ tests/test_utils.py
@@ -0,0 +1,8 @@
+from kevinbotlib.util import fullclassname
+
+class Test:
+    pass
+
+def test_fullclassname():
+    assert fullclassname(str) == "str"
+    assert fullclassname(Test) == "tests.test_utils.Test"
