f95262d5fdd76cc5a27d86ffb9341b1259217edb
meowmeowahr <meowmeowahr@gmail.com>
2025-03-04 23:29:14 -0600

    state button theming

--- src/kevinbotlib/apps/control_console/pages/control.py
+++ src/kevinbotlib/apps/control_console/pages/control.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QGridLayout, QListWidget, QPushButton
+from PySide6.QtCore import QSize
 
 
 class ControlConsoleControlTab(QWidget):
@@ -15,10 +16,16 @@ class ControlConsoleControlTab(QWidget):
         root_layout.addLayout(self.enable_layout)        
 
         self.enable_button = QPushButton("Enable")
+        self.enable_button.setObjectName("EnableButton")
+        self.enable_button.setFixedSize(QSize(128, 84))
         self.enable_layout.addWidget(self.enable_button, 0, 0)
 
         self.disable_button = QPushButton("Disable")
+        self.disable_button.setObjectName("DisableButton")
+        self.disable_button.setFixedSize(QSize(128, 84))
         self.enable_layout.addWidget(self.disable_button, 0, 1)
 
         self.estop_button = QPushButton("EMERGENCY STOP")
+        self.estop_button.setObjectName("EstopButton")
+        self.estop_button.setFixedHeight(96)
         self.enable_layout.addWidget(self.estop_button, 1, 0, 1, 2)
