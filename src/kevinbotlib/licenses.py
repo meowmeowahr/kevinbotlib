63efdeedce90ccb3e9d4e3cfca4d725a75fdea47
meowmeowahr <meowmeowahr@gmail.com>
2025-03-16 20:11:30 -0500

    licenses: add qtpy

--- src/kevinbotlib/licenses.py
+++ src/kevinbotlib/licenses.py
@@ -1138,6 +1138,30 @@ permanent authorization for you to choose that version for the
 Library.
 """
 
+_QTPY_LICENSE = """
+The MIT License (MIT)
+
+Copyright (c) 2011- QtPy contributors and others (see AUTHORS.md)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 _QTAWESOME_LICENSE = """
 The MIT License
 
@@ -1190,5 +1214,6 @@ def get_licenses() -> dict[str, str]:
         "shiboken6": _SHIBOKEN6_LICENSE,
         "websockets": _WEBSOCKETS_LICENSE,
         "ansi2html": _ANSI2HTML_LICENSE,
+        "qtpy": _QTPY_LICENSE,
         "qtawesome": _QTAWESOME_LICENSE,
     }
