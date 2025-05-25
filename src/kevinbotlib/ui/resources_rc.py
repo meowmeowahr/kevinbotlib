9e397cc5af859fe46b67cb3d1b248b45b83fea48
meowmeowahr <meowmeowahr@gmail.com>
2025-05-25 00:21:28 -0500

    cleanup

--- src/kevinbotlib/ui/resources_rc.py
+++ src/kevinbotlib/ui/resources_rc.py
@@ -3539,10 +3539,13 @@ qt_resource_struct = b"\
 \x00\x00\x01\x96\xe1\x9bO\x17\
 "
 
+
 def qInitResources():
     QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
 
+
 def qCleanupResources():
     QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
 
 qInitResources()
