df40333ee8205bacf8f674efbb721dd3bd32af24
meowmeowahr <meowmeowahr@gmail.com>
2025-06-15 22:00:42 -0500

    comm: remove native socket comms

--- examples/comm/native/server.py
+++
@@ -1,8 +0,0 @@
-from kevinbotlib.comm.core.server import NetworkServer
-from kevinbotlib.logger import Logger, LoggerConfiguration
-
-if __name__ == "__main__":
-    Logger().configure(LoggerConfiguration())
-
-    server = NetworkServer("127.0.0.1", 8888)
-    server.run()
